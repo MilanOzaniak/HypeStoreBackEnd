def74a58755a6b071e82ca784f5182ee2d4486e5
MilanOzaniak <71782252+MilanOzaniak@users.noreply.github.com>
2023-03-05 23:21:06 +0100

    cross origin

--- src/main/java/com/example/hypestore/controller/AuthController.java
+++ src/main/java/com/example/hypestore/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     @Autowired
--- src/main/java/com/example/hypestore/controller/CommentController.java
+++ src/main/java/com/example/hypestore/controller/CommentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/comment")
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CommentController {
 
     @Autowired
