f6ba6ec9e45ed1bfe5677fffee2a28b301649659
MilanOzaniak <71782252+MilanOzaniak@users.noreply.github.com>
2023-03-08 14:09:20 +0100

    asd

--- src/main/java/com/example/hypestore/controller/ItemController.java
+++ src/main/java/com/example/hypestore/controller/ItemController.java
@@ -19,7 +19,7 @@ import org.springframework.http.ResponseEntity;
 
 @RequestMapping("/item")
 @RestController
-@CrossOrigin(origins = "https://milanozaniak.github.io/")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class ItemController {
 
     @Autowired
